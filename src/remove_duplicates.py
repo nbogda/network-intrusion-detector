a72c86dc3c5c0e22ec189c8a46551b4acf1dc165
Charlie <chazzo96@yahoo.com>
2020-03-12 12:27:12 -0400

    Fucking sets man

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -1,20 +1,24 @@
-#!/usr/bin/python3
 import csv
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
 
 def main():
 
-	f=open("kddcup.data","r")
-	line_set={"0"}
-	while True:
-		line=f.readline()
-		if line =="":
-			break
-		line=line[:-2]
-		line_set.add(line+"\n")
-	line_set.remove("0")
-	w = open("../data/noduplicateskddcup.csv", "a")
-	for i in line_set:
-		w.write(i)
+    f=open("../data/train_data","r")
+    line_set={"0"}
+    while True:
+        line=f.readline()
+        if line =="":
+            break
+        line=line[:-2]
+        line_set.add(line)
+    line_set.remove("0")
+    
+    line_set = pd.DataFrame(list(line_set))
+    enc = OrdinalEncoder()
+    line_set.iloc[:,1:4] = enc.fit_transform(line_set.iloc[:,1:4])
+    line_set.to_csv("../data/train_nodup.csv",index=False)  
 
 if __name__ == "__main__":
-	exit(main())
+    exit(main())
