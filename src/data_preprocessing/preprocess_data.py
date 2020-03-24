09e6aa2c48e16e413af9dc5ec1f549d95e3388f1
Natalie Bogda <nbogda@vols.utk.edu>
2020-03-24 10:22:05 -0400

    Preprocessed data, PCA'd, for hello world

--- src/data_preprocessing/preprocess_data.py
+++ src/data_preprocessing/preprocess_data.py
@@ -1,17 +1,10 @@
 '''
-The following is from the Jantz man himself:
-
-"Suggestions for “Helloworld”: Deploy your data set on the Neuro-
-Firewall cluster. Train and test a single (simple) classifier, such as k-
-means clustering."
-
-And what Jantz says, goes.
+I made this a separate file because we are probably going to use it in other parts of our project
 '''
 
 import os
 import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler 
 from sklearn.decomposition import PCA
@@ -90,37 +83,7 @@ def main():
     	data = pd.read_csv('../data/train_nodup.csv')
     	X_train, X_test, y_train, y_test = split_train_test(data, pca=pca)
         print("Transformed data")
-    else:
-        desc = ""
-        if pca:
-            desc = "_PCA"
-        X_train = np.load("X_train%s.npy" % desc)
-        X_test = np.load("X_test%s.npy" % desc)
-        y_train = np.load("y_train%s.npy" % desc)
-        y_test = np.load("y_test%s.npy" % desc)
-        print("Loaded in data")
-
-    
-
-    
-
 
 if __name__ == "__main__":
     main()
-
-'''
-Plans for "Hello World":
-    1. Split the above data into train and test (70% / 30%)
-        1.a. Do we need to normalize the data?
-    2. Fit to kmeans cluster
-    3. Perform k-fold cross validation (either 3 or 5 folds, depends on how long it takes to run)
-    4. Get performance metrics, such as:
-        a. Accuracy
-        b. F1 Score
-        c. Confusion Matrix
-        d. ROC curve 
-        e. Cluster graph
-
-'''
-
 
